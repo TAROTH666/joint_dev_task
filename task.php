d312a990ba2e0c0814114b72332f4f65d35fa8b1
Troth IT <taroth827@gmail.com>
2020-10-28 23:46:26 +0900

    PHP応用課題Q17~21を完了

--- task.php
+++ task.php
@@ -387,3 +387,22 @@ foreach ($humans as $human) {
 }
 
 echo PHP_EOL;
+
+print("#####q20#####" . PHP_EOL);
+
+for ($i = 1; $i < 31; $i++) {
+
+  if ($i % 3 == 0 && $i % 5 == 0) {
+    echo "FizzBuzz" . PHP_EOL;
+  } elseif ($i % 3 == 0 && $i % 7 == 0) {
+    echo "FizzHoge" . PHP_EOL;
+  } elseif ($i % 3 == 0) {
+    echo "Fizz" . PHP_EOL;
+  } elseif ($i % 5 == 0) {
+    echo "Buzz" . PHP_EOL;
+  } elseif ($i % 7 == 0) {
+    echo "Hoge" . PHP_EOL;
+  } else {
+    echo $i . PHP_EOL;
+  }
+}
